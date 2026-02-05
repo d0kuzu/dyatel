3703f1feb1a405e8d416f1cb66fdaea6955e921c
d0kuzu <dkobdabaev@mail.ru>
2026-02-05 16:14:22 +0500

    docker and req

--- server.py
+++ server.py
@@ -29,4 +29,4 @@ def list_photos():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("server:app", host="127.0.0.1", port=8002, reload=True)
